ff9a253ca3e3ee396b440d32f667f95f5be76a71
mayanhu <mayanhu@xianhuaqian.cn>
2021-02-08 15:56:03 +0800

    feat : step4.Opengl的视口中代码的执行原理，以及引入opengl状态机的概念， opengl中的画布ViewPort

--- app/src/main/java/com/myh/learnogles/MainActivity.java
+++ app/src/main/java/com/myh/learnogles/MainActivity.java
@@ -17,19 +17,24 @@ class MayhGLSurfaceViewRenderer implements GLSurfaceView.Renderer{
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        //设置颜色
+        //设置颜色：在gl的渲染环境中设置ClearColor状态
         gl.glClearColor(0.1f,0.4f,0.6f,1.0f);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
+        //ViewPort是gl的另一个状态 ， ViewPort：画布  原点左下角（0，0），，等价Canvas
         gl.glViewport(0,0,width,height);
 
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT);
+        //擦出某些东西，  擦出（颜色缓存区）GL_COLOR_BUFFER_BIT
+        //GL_DEPTH_BUFFER_BIT 深度缓冲区
+        //GL_STENCIL_BUFFER_BIT 模板缓冲
+        //open从GL状态机中获取对应的状态值进行擦出
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT|gl.GL_DEPTH_BUFFER_BIT|gl.GL_STENCIL_BUFFER_BIT);
     }
 }
 class MayhGLSurfaceView extends GLSurfaceView{
