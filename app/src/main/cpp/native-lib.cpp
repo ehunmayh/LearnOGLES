f3a289a3a688638dc09d9a9fd00d8f51ca6ed840
mayanhu <mayanhu@xianhuaqian.cn>
2021-02-08 09:55:38 +0800

    feat :int work , start use jni

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,17 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_myh_learnogles_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+//  指针  java 对象  当前，  类似this
+extern "C" JNIEXPORT jint Java_com_myh_learnogles_MainActivity_Test(
+        JNIEnv *env, jobject MainActivity){
+        return 100;
+
+}
